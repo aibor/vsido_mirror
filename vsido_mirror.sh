a9ad94bad3d239e67f95ddad8d262f2e4bc0f0ae
Tobias Böhm <aibo@aibor.de>
2013-11-24 11:26:46 +0100

    removed useless IMAGES variable and tidied up the code a bit

--- vsido_mirror.sh
+++ vsido_mirror.sh
@@ -32,7 +32,8 @@ CHECKSUM_FILE_EXTENSION='.md5'
 CHECKSUM_COMMAND='/usr/bin/md5sum -c'
 MAKE_TORRENT_COMMAND="/usr/bin/btmakemetafile http://tracker.aibor.de:46969/announce \
     --announce_list http://tracker.aibor.de:46969/announce|http://bttracker.crunchbanglinux.org:6969/announce"
-    #|udp://tracker.ccc.de:80/announce,udp://tracker.openbittorrent.com:80/announce'"
+
+trap "{ quit 7; }" TERM INT KILL
 
 # make temporary working dir
 WORKING_DIR="$(/bin/mktemp -d)"
@@ -44,12 +45,23 @@ EXIT[3]='Failed to fetch file'
 EXIT[4]='Something went wrong, file isn`t here'
 EXIT[5]='checksum doesn`t match'
 EXIT[6]='Failed to create torrent'
+EXIT[7]='Aborting.'
+EXIT[8]='Failed to move files'
 
 # graceful quitting function
 quit() {
-  [ -z "${EXIT[$1]}" ] || /bin/echo "${EXIT[${1}]}${2+ --> ${2}} " 
+  [ -z "${EXIT[$1]}" ] || /bin/echo "${EXIT[${1}]}${2:+ --> ${2}} " 
   /bin/rm -r "${WORKING_DIR}"
   exit ${1}
+}
+
+fetch() {
+  # fetch the passed file
+  /usr/bin/wget -q "${1}" > /dev/null || quit 3 "${1##*/}"
+
+  # check if download of the file succeeded
+  [ -r "${1##*/}" ] || return 1
+  return 0
 }
 
 # enter working dir
@@ -64,9 +76,6 @@ iso_files="$(/bin/egrep -io 'http[^ ]*\.iso' <<< "${page}" | /usr/bin/uniq )"
 # quit if no isos have been found
 [ -z "${iso_files}" ] && quit 2
 
-# make empty array for all iso images
-declare -a IMAGES
-
 # loop through every found iso image
 for i in ${iso_files}
 do
@@ -74,43 +83,34 @@ do
   image="${i##*/}"
   checksum_file="${image/%/${CHECKSUM_FILE_EXTENSION}}"
 
-  # fill our array with the file names
-  IMAGES+=("${image}")
-
   # fetch checksum files for the images
-  /usr/bin/wget "${DOWNLOAD_URL}${checksum_file}" > /dev/null 2>&1 || quit 3 "${checksum_file}"
-
-  # check if download of checksum file succeeded
-  [ -r "${checksum_file}" ] || quit 4 "${checksum_file}"
+  fetch "${DOWNLOAD_URL}${checksum_file}" || quit 4 "${checksum_file}"
 
   # delete checksum file and continue with next image, if the md5 is present and identical in target directory
   if /usr/bin/diff -N {./,"${TARGET_DIR}"}"${checksum_file}" >/dev/null 
   then
     rm -f "${checksum_file}"
     continue
-  else
-    /bin/echo "Fetching and processing '${image}'"
-    /bin/echo 'This may take some time.'
   fi
 
-  # fetch the image 
-  /usr/bin/wget "${DOWNLOAD_URL}${image}" > /dev/null 2>&1 || quit 3 "${image}"
+  /bin/echo "Fetching and processing '${image}'"
+  /bin/echo 'This may take some time.'
 
-  # check if download of image succeeded
-  [ -r "${image}" ] || quit 4 "${image}"
+  # fetch the image 
+  fetch "${DOWNLOAD_URL}${image}" || quit 4 "${image}"
 
   # check checksum of image
-  ${CHECKSUM_COMMAND} "${checksum_file}" > /dev/null 2>&1 || quit 5 "${image}"
+  ${CHECKSUM_COMMAND} "${checksum_file}" > /dev/null || quit 5 "${image}"
 
   # create torrent file for the freshly downloaded image
-  ${MAKE_TORRENT_COMMAND} "${image}" > /dev/null 2>&1 || quit 6
+  ${MAKE_TORRENT_COMMAND} "${image}" > /dev/null || quit 6
 
   # check if torrent file is present
   [ -r "${image/%/.torrent}" ] || quit 4 "${image/%/.torrent}" 
 
   # change owner of all files and move them to the target dir
   /bin/chown "${TARGET_USER}":"${TARGET_USER}" "${image}"*
-  /bin/mv "${image}"* "${TARGET_DIR}"
+  /bin/mv "${image}"* "${TARGET_DIR}" || quit 8 "'${image}' to '${TARGET_DIR}'"
 
   # give response
   /bin/echo "downloaded '${image}', created torrent and moved all into '${TARGET_DIR}'"
